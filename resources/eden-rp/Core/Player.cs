b5d7fb7282e426da92bd2d671571dd524d41f791
Baran Daşdemir <dasdemirbaran@gmail.com>
2017-08-30 19:25:43 +0300

    seperated player/character classes
    well they're still connected in a way

---
+++ resources/eden-rp/Core/Player.cs
@@ -0,0 +1,30 @@
+﻿using GrandTheftMultiplayer.Server.Elements;
+using Eden.Character;
+
+namespace Eden.Core
+{
+    public class Player
+    {
+        Client client;
+        EdenCharacter character;
+        int clientid;
+        bool firstlogin;
+        int level;
+        int experience;
+        int adminlevel;
+
+        public Client Client { get { return client; } set { client = value; } }
+        public EdenCharacter Character { get { return character; } set { character = value; } }
+        public int Clientid { get { return clientid; } set { clientid = value; } }
+        public bool Firstlogin { get { return firstlogin; } set { firstlogin = value; } }
+        public int Level { get { return level; } set { level = value; } }
+        public int Experience { get { return experience; } set { experience = value; } }
+        public int Adminlevel { get { return adminlevel; } set { adminlevel = value; } }
+
+        public Player(Client player)
+        {
+            Client = player;
+            Character.Name = Client.name.Replace("_", " ");
+        }
+    }
+}
--- resources/eden-rp/character/Character.cs
+++ resources/eden-rp/character/Character.cs
@@ -1,9 +1,19 @@
-﻿using GrandTheftMultiplayer.Server.API;
-
-namespace eden_rp.character
+﻿namespace Eden.Character
 {
-    class Character
+    public class EdenCharacter
     {
+        double money;
+        int bankaccount;
+        double bankmoney;
+        int skin;
+        int age;
+        string name;
 
+        public double Money { get { return money; } set { money = value; } }
+        public int Bankaccount { get { return bankaccount; } set { bankaccount = value; } }
+        public double Bankmoney { get { return bankmoney; } set { bankmoney = value; } }
+        public int Skin { get { return skin; } set { skin = value; } }
+        public int Age { get { return age; } set { age = value; } }
+        public string Name { get { return name; } set { name = value; } }
     }
 }
