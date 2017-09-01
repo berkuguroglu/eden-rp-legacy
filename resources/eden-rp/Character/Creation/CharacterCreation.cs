69e989473bcfe3665478b9474ae92c3c91aafa2a
Baran Daşdemir <dasdemirbaran@gmail.com>
2017-09-01 05:37:30 +0300

    added early stage character creation code

---
+++ resources/eden-rp/Character/Creation/CharacterCreation.cs
@@ -0,0 +1,19 @@
+﻿using GrandTheftMultiplayer.Server.Constant;
+using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Shared.Math;
+using Eden.Core;
+
+namespace Eden.Character.Creation
+{
+    public static class CharacterCreation
+    {
+        public static void InitializeCreator(Player player)
+        {
+            player.Client.position = new Vector3(402.9086, -996.8259, -99.00024);
+            player.Client.rotation = new Vector3(0.0, 0.0, -177.6333);
+            player.Client.playAnimation("mp_character_creation@customise@male_a", "drop_loop", 1);
+            player.Client.freeze(true);
+            player.Client.triggerEvent("CCMain", player.Character.Gender, player.Character.Age);
+        }
+    }
+}
