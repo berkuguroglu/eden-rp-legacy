722828ed62aa3e6e8ea81de365aecaa66f164d88
Baran Daşdemir <dasdemirbaran@gmail.com>
2017-09-01 15:27:33 +0300

    changed hud visibility during login
    hud is not visible during login anymore

--- resources/eden-rp/core/login/login.js
+++ resources/eden-rp/core/login/login.js
@@ -4,6 +4,7 @@ API.onServerEventTrigger.connect(function (eventName, args) {
 
     switch (eventName) {
         case "login": {
+            API.setHudVisible(false);
             res = API.getScreenResolution();
             loginBrowser = API.createCefBrowser(400, 300, true);
             API.waitUntilCefBrowserInit(loginBrowser);
@@ -16,6 +17,7 @@ API.onServerEventTrigger.connect(function (eventName, args) {
             API.sendNotification("Giris basarili!");
             API.showCursor(false);
             API.destroyCefBrowser(loginBrowser);
+            API.setHudVisible(true);
             API.setActiveCamera(null);
             break;
         }
