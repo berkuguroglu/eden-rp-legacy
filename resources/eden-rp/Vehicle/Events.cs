4a3cd834d6b5fcc3d85565133f8aac7e542819e3
Expirty <expirty.bu@gmail.com>
2017-09-01 18:35:17 +0300

    recommit

--- resources/eden-rp/Vehicle/Events.cs
+++ resources/eden-rp/Vehicle/Events.cs
@@ -1,6 +1,10 @@
 ﻿using Eden.Core;
 using GrandTheftMultiplayer.Server.Elements;
 using GrandTheftMultiplayer.Shared;
+using GrandTheftMultiplayer.Shared.Math;
+
+
+
 
 
 namespace Eden.Vehicle
@@ -10,11 +14,16 @@ namespace Eden.Vehicle
         public static void OnResourceStart() { DatabaseHandler.LoadVehicles(); }
         public static void OnPlayerEnterVehicle(Client player, NetHandle vehicle)
         {
-            foreach(EdenVehicle edh in EdenCore.VehicleList)
+            foreach (EdenVehicle edh in EdenCore.VehicleList)
             {
-                if(edh.Veh == vehicle)
+                if (edh.Veh == vehicle)
                 {
-                    EdenCore.api.sendChatMessageToPlayer(player, "~r~(([ID] ~w~" + edh.Vehid.ToString() + " ~r~|| ~w~" + edh.Modelhash.ToString() +"~r~ || Sahip: ~w~" + edh.Ownername + "~r~ ))");
+                    EdenCore.api.sendChatMessageToPlayer(player, "~r~(([ID] ~w~" + edh.Vehid.ToString() + " ~r~|| ~w~" + edh.Modelhash.ToString() + "~r~ || Sahip: ~w~" + edh.Ownername + "~r~ ))");
+                    if (edh.LockStatus)
+                    {
+                        EdenCore.api.setEntityPosition(player, new Vector3(EdenCore.api.getEntityPosition(edh.Veh).X + 3, EdenCore.api.getEntityPosition(edh.Veh).Y, EdenCore.api.getEntityPosition(edh.Veh).Z));
+                        EdenCore.api.sendChatMessageToPlayer(player, "~r~Bu araç kilitli.");
+                    }
                     break;
                 }
             }
--- resources/eden-rp/core/commands/Admin.cs
+++ resources/eden-rp/core/commands/Admin.cs
@@ -1,6 +1,8 @@
-﻿using GrandTheftMultiplayer.Server.API;
-using GrandTheftMultiplayer.Server.Managers;
+﻿using Eden.Core;
 using GrandTheftMultiplayer.Server.Elements;
+using GrandTheftMultiplayer.Server.Managers;
+using GrandTheftMultiplayer.Server.Constant;
+using GrandTheftMultiplayer.Server.API;
 
 namespace Eden.Core.Commands
 {
@@ -8,5 +10,42 @@ namespace Eden.Core.Commands
     {
         [Command("mylocation")]
         public void PrintPosition(Client player) { API.consoleOutput("Location information of {0}\nPosition\nX: {1} Y: {2} Z: {3}\nRotation\nX: {4} Y: {5} Z: {6}", player.name, player.position.X, player.position.Y, player.position.Z, player.rotation.X, player.rotation.Y, player.rotation.Z); }
+        [Command("gotoplayer", "~w~(( KULLANIM: /gotoplayer [clientid]))", GreedyArg = true)]
+        public void GoToPlayer(Client player, int id, int fix = 0)
+        {
+            if (Core.Player.Find(player).Adminlevel > 2)
+            {
+                Core.Player peh = Core.Player.Find(id);
+                if (peh == null) API.sendChatMessageToPlayer(player, "~r~Bu oyuncu oyunda değil.");
+                else API.setEntityPosition(player, peh.Client.position);
+            }
+        }
+        [Command("getplayer", "~w~(( KULLANIM: /getplayer [clientid]))", GreedyArg = true)]
+        public void GetPlayer(Client player, int id, int fix = 0)
+        {
+            if (Core.Player.Find(player).Adminlevel > 2)
+            {
+                Core.Player peh = Core.Player.Find(id);
+                if (peh == null) API.sendChatMessageToPlayer(player, "~r~Bu oyuncu oyunda değil.");
+                else API.setEntityPosition(peh.Client, player.position);
+            }
+
+        }
+        [Command("setskin", "~w~(( KULLANIM: /setskin [clientid] [pedhash]))", GreedyArg = true)]
+        public void SetPlayerSkin(Client player, int id, PedHash ped, int fix = 0)
+        {
+            if (Core.Player.Find(player).Adminlevel > 2)
+            {
+                Core.Player peh = Core.Player.Find(id);
+                if (peh == null) API.sendChatMessageToPlayer(player, "~r~Bu oyuncu oyunda değil.");
+                else
+                {
+                    API.setPlayerSkin(peh.Client, ped);
+                }
+            }
+
+        }
+
     }
+
 }
