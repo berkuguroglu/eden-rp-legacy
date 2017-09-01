51c4ab5bd4e6622d42d00ba2d167621b8af577ea
Baran Daşdemir <dasdemirbaran@gmail.com>
2017-09-01 15:24:25 +0300

    added gender property

--- resources/eden-rp/character/Character.cs
+++ resources/eden-rp/character/Character.cs
@@ -1,5 +1,4 @@
-﻿
-namespace Eden.Character
+﻿namespace Eden.Character
 {
     public class EdenCharacter
     {
@@ -9,6 +8,7 @@ namespace Eden.Character
         int skin;
         int age;
         string name;
+        bool gender;
 
         public double Money { get { return money; } set { money = value; } }
         public int Bankaccount { get { return bankaccount; } set { bankaccount = value; } }
@@ -16,5 +16,6 @@ namespace Eden.Character
         public int Skin { get { return skin; } set { skin = value; } }
         public int Age { get { return age; } set { age = value; } }
         public string Name { get { return name; } set { name = value; } }
+        public bool Gender { get { return gender; } set { gender = value; } }
     }
 }
